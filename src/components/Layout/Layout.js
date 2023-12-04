982dde16609eb46191b208f5d8ab9163330c7d51
fabian <fabian.salas.astete@gmail.com>
2023-12-04 17:21:06 -0300

    issues: update new design to carousel section and add button contact wsp

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,6 +7,7 @@ import Properties from '../../pages/Properties/Properties';
 import About from '../../pages/About';
 import Head from '../../components/Head/Head';
 
+import ContactWsp from '../ButtonContact/Fab';
 const Layout = () => {
   return (
     <div className="relative mt-16 xl:mt-24 overflow-hidden w-100 bg-white">
@@ -23,6 +24,7 @@ const Layout = () => {
       <Properties/>
       {/* <Property/> */}
       <About/>
+      <ContactWsp/>
       {/* FOOTER PAGE */}
       <Footer />    
     </div>
--- src/components/PageSections/About/index.js
+++ src/components/PageSections/About/index.js
@@ -13,8 +13,8 @@ const AboutComponent = () => {
   return (
     <Section>
     {/* <Fade delay={300} direction="right"> */}
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3  gap-4">
-      <div className="col-span-1 md:col-span-1 xl:col-span-2 flex flex-col justify-center mx-2 xl:ml-24">
+    <div className="grid grid-cols-1 md:grid-cols-3 xl:grid-cols-3  gap-1">
+      <div className="col-span-1 md:col-span-1 xl:col-span-2 flex flex-col justify-center mx-2 xl:ml-6">
         <h2 className="text-6xl xl:text-5xl text-center  text-primary font-bold">
             Sobre mí
           </h2>
@@ -31,7 +31,7 @@ const AboutComponent = () => {
             :null}
           </div>
           <div className="col-span-2 xl:col-span-1 flex flex-col">
-            <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-5 md:text-center  xl:text-left">
+            <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-1  xl:text-left">
             Con más de una década de experiencia, hemos brindado asesoramiento especializado a beneficiarios del Subsidio DS1 en la adquisición de propiedades usadas, gestionando integralmente el proceso de compraventa. Esto incluye desde la tasación inicial hasta el estudio exhaustivo de títulos, la redacción de escrituras de compraventa y la gestión de la inscripción en el Conservador de Bienes Raíces. Además, nos encargamos de coordinar los pagos correspondientes al vendedor, asegurando así una gestión completa y efectiva del proceso de transacción inmobiliaria
               <br />
               <br />
@@ -45,10 +45,10 @@ const AboutComponent = () => {
               <br /> */}
             </p>
 
-            <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-5 md:text-center xl:text-left">
+            <p className="text-lg xl:text-xl text-gray-700 mt-8 ml-0 xl:ml-1 xl:text-left">
               Asesoria A & M.
             </p>
-            <p className="text-xl xl:text-xl text-gray-700 ml-0 xl:ml-5 md:text-center xl:text-left">
+            <p className="text-xl xl:text-xl text-gray-700 ml-0 xl:ml-1 xl:text-left">
               Fundador y Gerente General.
             </p>
           </div>
